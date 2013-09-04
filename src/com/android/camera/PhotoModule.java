1b59752625ce2af87fa390a05ddfae7c73515aa0
Lars Greiss <kufikugel@googlemail.com>
2013-09-04 22:30:31 +0200

    Camera: add Smart capture
    add back smart capture feature. Recoded and addapt to the
    new aosp camera. This time each camera and video can set it
    seperatly.
    
    drawables by kbeezie
    
    Change-Id: I87a895c21c53d11cab573df4fd76f1628ca03b1a

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -481,6 +481,7 @@ public class PhotoModule
         mFocusManager.setMirror(mMirror);
         mFocusManager.setParameters(mInitialParams);
         setupPreview();
+        initSmartCapture();
 
         // reset zoom value index
         mZoomValue = 0;
@@ -1239,6 +1240,8 @@ public class PhotoModule
         UsageStatistics.onContentViewChanged(
                 UsageStatistics.COMPONENT_CAMERA, "PhotoModule");
 
+        initSmartCapture();
+
         Sensor gsensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         if (gsensor != null) {
             mSensorManager.registerListener(this, gsensor, SensorManager.SENSOR_DELAY_NORMAL);
@@ -1262,6 +1265,8 @@ public class PhotoModule
         if (msensor != null) {
             mSensorManager.unregisterListener(this, msensor);
         }
+
+        stopSmartCapture();
     }
 
     @Override
@@ -1483,6 +1488,28 @@ public class PhotoModule
         // Change the camera display orientation
         if (mCameraDevice != null) {
             mCameraDevice.setDisplayOrientation(mCameraDisplayOrientation);
+        }
+    }
+
+    private void initSmartCapture() {
+        if (mActivity.initSmartCapture(mPreferences, false)) {
+            startSmartCapture();
+        } else {
+            stopSmartCapture();
+        }
+    }
+
+    private void startSmartCapture() {
+        Sensor psensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        if (psensor != null) {
+            mSensorManager.registerListener(this, psensor, SensorManager.SENSOR_DELAY_UI);
+        }
+    }
+
+    private void stopSmartCapture() {
+        Sensor psensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        if (psensor != null) {
+            mSensorManager.unregisterListener(this, psensor);
         }
     }
 
@@ -1845,6 +1872,7 @@ public class PhotoModule
 
         setCameraParametersWhenIdle(UPDATE_PARAM_PREFERENCE);
         mUI.updateOnScreenIndicators(mParameters, mPreferenceGroup, mPreferences);
+        initSmartCapture();
     }
 
     @Override
@@ -1969,6 +1997,17 @@ public class PhotoModule
             data = mGData;
         } else if (type == Sensor.TYPE_MAGNETIC_FIELD) {
             data = mMData;
+        } else if (type == Sensor.TYPE_PROXIMITY) {
+            int currentProx = (int) event.values[0];
+            if (currentProx == 0) {
+                if (mFirstTimeInitialized) {
+                    onShutterButtonFocus(true);
+                }
+                if (canTakePicture()) {
+                    onShutterButtonClick();
+                }
+            }
+            return;
         } else {
             // we should not be here.
             return;
