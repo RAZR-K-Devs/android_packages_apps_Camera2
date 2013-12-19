SlimRoms/packages_apps_Gallery2

Camera: add color effects
drawables by kbeezie

Change-Id: I389d621e73bd14e6c85c62edc75c93b5282d4c3a

src/com/android/camera/PhotoModule.java
@@ -1934,6 +1934,14 @@ public class PhotoModule
         // For the following settings, we need to check if the settings are
         // still supported by latest driver, if not, ignore the settings.
 
+        // Color effect
+        String colorEffect = mPreferences.getString(
+                CameraSettings.KEY_COLOR_EFFECT,
+                mActivity.getString(R.string.pref_camera_coloreffect_default));
+        if (Util.isSupported(colorEffect, mParameters.getSupportedColorEffects())) {
+            mParameters.setColorEffect(colorEffect);
+        }
+
         // Set exposure compensation
         int value = CameraSettings.readExposure(mPreferences);
         int max = mParameters.getMaxExposureCompensation();

RAZR-K-Devs/android_packages_apps_Camera2

src/com/android/camera/PhotoModule.java
@@ -1731,6 +1731,14 @@ public class PhotoModule
         // For the following settings, we need to check if the settings are
         // still supported by latest driver, if not, ignore the settings.
 
+        // Color effect
+        String colorEffect = mPreferences.getString(
+                CameraSettings.KEY_COLOR_EFFECT,
+                mActivity.getString(R.string.pref_camera_coloreffect_default));
+        if (CameraUtil.isSupported(colorEffect, mParameters.getSupportedColorEffects())) {
+            mParameters.setColorEffect(colorEffect);
+        }
+
         // Set exposure compensation
         int value = CameraSettings.readExposure(mPreferences);
         int max = mParameters.getMaxExposureCompensation();